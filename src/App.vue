e2daf975c254d4787939ce91101281e0e0124e98
RuxandraRambet <ruxandraramb@gmail.com>
2023-12-06 16:04:58 +0100

    feat: exercise

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    AppTitle,
+  },
+
+};
+</script>
+
+<template>
+  <AppTitle />
+  
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
